b564e8d7a0af0b2d6be8c930b3a49061e58d8ee8
ionasNeonakis <ionas.neonakis@gmail.com>
2020-11-18 09:30:45 +0100

    Ajout de la doc

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from collections import deque
 RES_DIR = "res/"
 AVAILABLE_COLORS = set(['blue', 'red', 'green', 'white', 'black'])
 
+# Supprime les espaces inutiles et les sauts de ligne
 def clean_string(s):
     return s.strip().replace('\n', '')
 
+# Retourne un graphe à partir d'un fichier .graphe qui se trouve dans RES_DIR
 def parse_graph(filename):
     graph = {}
     graphLength = -1
@@ -33,9 +35,11 @@ def parse_graph(filename):
 
     return graph
 
+# Calcule le degré d'un sommet vertex dans un graphe
 def degree(graph, vertex):
     return len(graph[vertex])
 
+# Retourne les couleurs de tous les sommets neighbours
 def neighbourhood_colors(neighbours, coloring):
     used_colors = set()
    
@@ -44,6 +48,7 @@ def neighbourhood_colors(neighbours, coloring):
 
     return used_colors
 
+# Supprime un sommet d'un graphe en se supprimant des liste d'adjacence des autres sommets
 def remove_vertex(graph, vertex):
     for v in graph:
         if vertex in graph[v]:
@@ -51,13 +56,14 @@ def remove_vertex(graph, vertex):
             
     return graph.pop(vertex)
 
-# Initie la couleur de chaque sommet à une couleur au 'hasard'
+# Initie la couleur de chaque sommet à une couleur au 'hasard'. Chaque sommet aura la même couleur
 def init_colors(graph):
     coloring = {}
     for v in graph.keys():
         coloring[v] = next(iter(AVAILABLE_COLORS))
     return coloring
 
+# Trouve un chemin d'un graphe, à partir de starting_vertex jusqu'à vertex_to_find
 def get_path(graph, starting_vertex, vertex_to_find):
     visited = []
     queue = deque()
@@ -75,6 +81,7 @@ def get_path(graph, starting_vertex, vertex_to_find):
     
     return False
 
+# Parcours en profondeur à partir de starting_vertex
 def breadth_first_search(graph, starting_vertex):
     visited = []
     queue = deque()
@@ -89,6 +96,7 @@ def breadth_first_search(graph, starting_vertex):
     
     return visited
 
+# Inverse les couleurs a et b d'un graphe. Coloring est les couleurs acctuelle du graphe
 def inverse_color(graph, coloring, color_a, color_b):
     for vertex in graph:
         if coloring[vertex] == color_a:
@@ -97,16 +105,17 @@ def inverse_color(graph, coloring, color_a, color_b):
             coloring[vertex] = color_a
     return coloring
 
+# Retourne une 5 coloration d'un graphe
 def coloring_rec(graph, coloring):
-
     if graph:
         for vertex in graph:
             if degree(graph, vertex) <= 5:
-                x = vertex
+                x = vertex # on trouve un sommet de degré <= 5 
                 break
+
         deg_x = degree(graph, x)
-        neighbours = remove_vertex(graph, x)
-        coloring_res = coloring_rec(graph, coloring)
+        neighbours = remove_vertex(graph, x) # on supprime le sommet x et récupère son sommet
+        coloring_res = coloring_rec(graph, coloring) # appel récursif
 
         if deg_x < 5:
             # On applique la brique 4
@@ -121,7 +130,7 @@ def coloring_rec(graph, coloring):
             if len(used_colors) < 5:
                 # On applique la brique 5
                 remaining_colors = AVAILABLE_COLORS - neighbourhood_colors(neighbours, coloring_res)
-                coloring[x] = next(iter(remaining_colors))
+                coloring[x] = next(iter(remaining_colors)) # x prend la première couleur disponible
             else:
                 # On applique la brique 6
                 a = neighbours[0]
@@ -136,28 +145,28 @@ def coloring_rec(graph, coloring):
                 delta   = coloring[d]
                 epsilon = coloring[e]
 
-                induced_subgraph_ag = {}
-                induced_subgraph_bd = {}
+                induced_subgraph_ag = {} # graphe induit de alpha gamma
+                induced_subgraph_bd = {} # graphe induit de beta delta
 
-                for k,v in graph.items():
+                for k,v in graph.items(): # on trouves les deux graphes induits 
                     if coloring_res[k] == alpha or coloring_res[k] == gamma:
                         induced_subgraph_ag[k] = v
                     if coloring_res[k] == beta or coloring_res[k] == delta:
                          induced_subgraph_bd[k] = v
 
-                if get_path(induced_subgraph_ag, a, c):
-                    connected_component = breadth_first_search(induced_subgraph_bd, b)
-                    new_coloring = inverse_color(connected_component, coloring_res, beta, delta)
-                else:
-                    connected_component = breadth_first_search(induced_subgraph_ag, a)
-                    new_coloring = inverse_color(connected_component, coloring_res, alpha, gamma)
+                if get_path(induced_subgraph_ag, a, c): # s'il y a un chemin de a vers c
+                    connected_component = breadth_first_search(induced_subgraph_bd, b) # on prend la composante connexe G'(beta, delta) qui contient b
+                    new_coloring = inverse_color(connected_component, coloring_res, beta, delta)# on inverse les couleurs de G'(beta, delta)
+                else: # S'il y a un chemin de b à d ou qu'il n'y a pas de chemin de a à c et de b à d
+                    connected_component = breadth_first_search(induced_subgraph_ag, a) # on prend la composante connexe G'(alpha, gamma) qui contient a
+                    new_coloring = inverse_color(connected_component, coloring_res, alpha, gamma)# on inverse les couleurs de G'(alpha, gamma)
 
-                remaining_colors = AVAILABLE_COLORS - neighbourhood_colors(neighbours, new_coloring)
-                coloring[x] = next(iter(remaining_colors))
+                remaining_colors = AVAILABLE_COLORS - neighbourhood_colors(neighbours, new_coloring) # on récupère la couleur restante
+                coloring[x] = next(iter(remaining_colors)) # on la donne à x
 
     return coloring
 
-
+# Vérifie que la coloration d'un graphe est correcte
 def check_coloring(graph, coloring):
     for v,e in graph.items():
         self_color = coloring[v]
